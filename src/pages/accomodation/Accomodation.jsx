837357e308af09bbf57ca2c2132cbf130f292844
jimmyDef <jimmydef@outlook.fr>
2024-01-22 16:04:53 +0100

    fix: erreur chemin fichier data.json

--- src/pages/accomodation/Accomodation.jsx
+++ src/pages/accomodation/Accomodation.jsx
@@ -13,7 +13,7 @@ function Accomodation() {
   const { id } = useParams();
 
   const { fetchedData, isLoading } = useFetch(
-    window.location.origin + "/daata.json"
+    window.location.origin + "/data.json"
   );
   //
   //Si Fetch OK : recherche de l'objet method .find()
